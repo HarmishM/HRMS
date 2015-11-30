a24e0132b7ff0d3106aa51bd91a945a4068f0be0
Harshil10 <harshil492@yahoo.com>
2015-11-30 20:43:36 +0530

    modified vew for above commit

--- management/hr/views/new_employee.py
+++ management/hr/views/new_employee.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import *
 @csrf_protect
 def feed_new_empl_details(request):
 	if request.method == 'GET':
-		return render(request, 'feed_employee.html')
+		return render(request, 'new-employee/feed_employee.html')
 	elif request.method == 'POST':
 		print 'In Here POST'
