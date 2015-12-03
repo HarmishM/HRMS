1de935650113b82fee0c7f4c03f0e4f1e49686c5
Harshil10 <harshil492@yahoo.com>
2015-12-04 01:44:24 +0530

    created view for detail info(personal) page

--- management/hr/views/new_employee.py
+++ management/hr/views/new_employee.py
@@ -4,6 +4,7 @@ from hr.models import *
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from datetime import datetime
+from django.http import HttpResponseNotFound, HttpResponse
 
 @csrf_protect
 def feed_new_empl_details(request):
@@ -17,13 +18,13 @@ def feed_new_empl_details(request):
 		contxt = {'emp_pk': '#'+str(curr_pk)}
 		return render(request, 'new-employee/feed_employee.html', contxt)
 	elif request.method == 'POST':
-		print 'In Here POST'
+		#print 'In Here POST'
 		new_emp_id = (request.POST.get('employee_code')).split('#')[1]
 		new_emp_firstname = request.POST.get('firstname')
 		new_emp_middlename = request.POST.get('middlename')
 		new_emp_lastname = request.POST.get('lastname')
 		new_emp_dob = request.POST.get('dob')
-		print new_emp_id, new_emp_firstname, new_emp_middlename, new_emp_lastname, new_emp_dob
+		#print new_emp_id, new_emp_firstname, new_emp_middlename, new_emp_lastname, new_emp_dob
 		date_obj = datetime.strptime(new_emp_dob, '%m/%d/%Y')
 		converted_date_obj = date_obj.strftime('%Y-%m-%d')
 
@@ -46,4 +47,31 @@ def feed_new_empl_details(request):
 		obj.empl_dob = converted_date_obj
 		obj.save()
 
-		return render(request, 'new-employee/employee-details.html')
+		return redirect(reverse('Full-employee-info', kwargs={'emp_id': new_emp_id}))
+
+''' fetches the employee's personal details '''
+def _get_personal_details(request_emp_id):
+	personal_cntx = {}
+	print request_emp_id
+	try:
+		emp_obj = CoreEmployee.objects.get(empl_id=request_emp_id)
+	except CoreEmployee.DoesNotExist:
+		print 'in exception'
+		return None
+	personal_cntx['fname'] = emp_obj.empl_fname
+	personal_cntx['mname'] = emp_obj.empl_mname
+	personal_cntx['lname'] = emp_obj.empl_lname
+	personal_cntx['dob'] = emp_obj.empl_dob
+	return personal_cntx
+
+@csrf_protect
+def full_employee_info(request, emp_id):
+	if request.method == 'GET':
+		print 'hit get full employee'
+		full_employee_cntxt = {}
+		'''Start getting the details of employee for received emp_id'''
+		personal_cntx = _get_personal_details(emp_id)
+		if personal_cntx is None:
+			return HttpResponse('<h3> Employee with this ID is not found </h3>')
+		full_employee_cntxt.update(personal_cntx)
+		return render(request, 'new-employee/employee-details.html', full_employee_cntxt)
