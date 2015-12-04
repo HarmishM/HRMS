f3b7f4609a37db623b9ccd5904a696d26877b3e5
Harshil10 <harshil492@yahoo.com>
2015-12-05 00:09:39 +0530

    test

--- management/hr/views/utils.py
+++ management/hr/views/utils.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
+''' converts date obtained from HTML to datetime object for storing in DB '''
 
-def _convert_date(date_obj):
+
+def convert_date(date_obj):
     intermediate_obj = datetime.strptime(date_obj, '%m/%d/%Y')
     modified_time_obj = intermediate_obj.strftime('%Y-%m-%d')
     return modified_time_obj
