2b37d363a73cc8f4bac8e61272345dc4b4545baf
Harshil <harshil492@yahoo.com>
2015-12-08 19:33:27 +0530

    Almost done except for Reporting module

--- management/hr/static/js/insert_job_details.js
+++ management/hr/static/js/insert_job_details.js
@@ -1,30 +1,92 @@
 
+function on_change(element){
+    //var element = $('#depts option:selected').text();
+    if(typeof element === undefined) {
+        alert('not defined');
+    }
+    // alert('element is: '+ element);
+	    $.ajax({
+		    type: 'POST',
+		    url: '/hr/api/ajax/departments/',
+		    data: {
+			    'department': element,
+		    },
+		    success: function(data){
+			    //console.log(data);
+			    //$('#desg').find('option:gt(0)').remove();
+			    // $('#desg').empty();
+			    $('#desg').find('option:gt(0)').remove();
+			    // alert('data is:'+ data);
+			    $.each(data, function(key, value){
+				    $('#desg').append('<option value='+value+'>'+value+'</option>');
+			    });
+			// $('#desg').prepend('<option value="None">Not Selected Yet</option>');
+			$('#depts').val(jobDepartment);
+			alert(jobType);
+            $('#desg').val(jobDesig); $('#emp-type').val(jobType); $('#join-date').val(joinDate); $('#job-location').val(jobLoc);
+			$('.selectpicker').selectpicker('refresh');
+		    },
+	    });
+
+};
+
+function on_change_new(element){
+    //var element = $('#depts option:selected').text();
+    if(typeof element === undefined) {
+        alert('not defined');
+    }
+    // alert('element is: '+ element);
+	    $.ajax({
+		    type: 'POST',
+		    url: '/hr/api/ajax/departments/',
+		    data: {
+			    'department': element,
+		    },
+		    success: function(data){
+			    //console.log(data);
+			    //$('#desg').find('option:gt(0)').remove();
+			    // $('#desg').empty();
+			    $('#desg').find('option:gt(0)').remove();
+			    // alert('data is:'+ data);
+			    $.each(data, function(key, value){
+				    $('#desg').append('<option value='+value+'>'+value+'</option>');
+			    });
+			// $('#desg').prepend('<option value="None">Not Selected Yet</option>');
+			$('.selectpicker').selectpicker('refresh');
+		    },
+	    });
+
+};
+
 function disable_job_detail(){
 
     function restore_job(){
+        alert('caleeddd second');
         // assign varibles received from backend contect here.
-        on_change($('#depts').val());
-        alert($('#depts').val());
-        // $('#depts').val(jobDesig);
-        $('#desg').val(jobDesig); $('#emp-type').val(jobType);
-        $('#join-date').val(joinDate); $('#job-location').val(jobLoc);
+        $('#job-edit').show(); $('#job-edit-save').hide(); $('#job-edit-cancel').hide();
+        // alert(jobDepartment+' / '+jobDesig);
+        // $('#depts').val(jobDepartment);
+        //alert($('#depts').text());
+        // $("#depts option:contains("+jobDepartment+")").attr('selected', 'selected');
+        // $('#emp-type').val(jobType);
+
+        $('.selectpicker').selectpicker('refresh');
+        on_change(jobDepartment.toString());
+		$('#depts, #desg').prop('disabled', true);
 
         // $("#depts option:contains(" + jobDepartment + ")").attr('selected', 'selected');
-        on_change($('#depts').val());
 
         // disable control here while freshly loaded, hide ``save`` and ``cancel`` buttons too...
-        $('#job-edit').show();
-        $('#job-edit-save').hide(); $('#job-edit-cancel').hide();
+
         $('#depts').prop('disabled', true); $('#desg').prop('disabled', true); $('#emp-type').prop('disabled', true);
         $('#join-date').prop('disabled', true); $('#job-location').prop('disabled', true);
-        $('.selectpicker').selectpicker('refresh');
 
     };
 
     $('#job-edit').on('click', function(){
         $('#job-edit').hide();
         $('#job-edit-save').show(); $('#job-edit-cancel').show();
-
+        alert('callllllleeeedddd');
         $('#depts').prop('disabled', false); $('#desg').prop('disabled', false); $('#emp-type').prop('disabled', false);
         $('#join-date').prop('disabled', false); $('#job-location').prop('disabled', false);
         $('.selectpicker').selectpicker('refresh');
@@ -38,6 +100,8 @@ function disable_job_detail(){
 };
 
 function fill_departments_designations() {
+    // alert('first caleedddddddddd');
+    // $("#depts option:contains(" + jobDepartment + ")").attr('selected', 'selected');
 	$.ajax({
 		type: 'GET',
 		url: '/hr/api/ajax/departments/',
@@ -51,44 +115,26 @@ function fill_departments_designations() {
 						});
 						break;
 					case 'desgs':
-					    if ($('#depst').val() == 'Engineering-Dev')
-						    {
-						        $.each(value, function(k, v){
-							        $('#desg').append('<option value='+v+'>'+v+'</option>');
-						        });
-						    }
+					    $.each(value, function(k, v){
+							$('#desg').append('<option value='+v+'>'+v+'</option>');
+						});
+
 						break;
 				}
+
 			$('.selectpicker').selectpicker('refresh');
+			// alert($('#depts').text());
 			});
 		},
 	});
 
 	$('#depts').change(function(){
-		//alert($('#depts option:selected').text());
-        on_change()
+		var elementSelected = $('#depts option:selected').text();
+        on_change_new(elementSelected);
 	});
+	disable_job_detail();
 };
 
-function on_change(element){
-    if (typeof(element)==='undefined') element = $('#depts').val();
-	    $.ajax({
-		    type: 'POST',
-		    url: '/hr/api/ajax/departments/',
-		    data: {
-			    'department': element,
-		    },
-		    success: function(data){
-			    //console.log(data);
-			    $('#desg').find('option:gt(0)').remove();
-			    $.each(data, function(key, value){
-				    $('#desg').append('<option value='+value+'>'+value+'</option>');
-			    });
-			$('.selectpicker').selectpicker('refresh');
-		    },
-	    });
-
-};
 
 function post_job_info(){
 
@@ -97,7 +143,8 @@ function post_job_info(){
         $('#loading').show();
         var employeeID = getEmployeeIdFromURL();
         var dep = $('#depts option:selected').text(); var desg = $('#desg option:selected').text(); var emType=$('#emp-type option:selected').text();
-        var joinD = $('#join-date').text(); var jobLocation = $('#job-location option:selected').text();
+        var joinD = $('#join-date').val(); var jobLocation = $('#job-location option:selected').text();
+        // alert(joinD);
         $.ajax({
             url: '/hr/api/ajax/job/update/',
             type: 'POST',
@@ -110,7 +157,10 @@ function post_job_info(){
                 jobDepartment = dep; jobDesig = desg; jobType = emType;
                 joinDate = joinD; jobLoc = jobLocation;
                 $('#loading').hide();
-                disable_job_detail();
+                $('#job-edit').show(); $('#job-edit-save').hide(); $('#job-edit-cancel').hide();
+                // $('#depts, #desg').prop('disabled', true);
+                $('#depts').prop('disabled', true); $('#desg').prop('disabled', true); $('#emp-type').prop('disabled', true);
+                $('#join-date').prop('disabled', true); $('#job-location').prop('disabled', true);
             },
         });
     });
